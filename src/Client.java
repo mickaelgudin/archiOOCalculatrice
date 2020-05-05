2f5330c1c4e94c197015a125f29733343d6eccaa
mickaelgudin <39730173+mickaelgudin@users.noreply.github.com>
2020-05-05 12:36:58 +0200

    internationalisation

--- src/Client.java
+++ src/Client.java
@@ -3,11 +3,22 @@ import java.net.UnknownHostException;
 
 import client.RequeteCalculatrice;
 import model.Addition;
+import settings.ApplicationProperties;
 import view.Interface;
 
 public class Client {
 
 	public static void main(String[] args) throws UnknownHostException, ClassNotFoundException, IOException, InterruptedException {
+		String lang = "fr";
+		
+		if(lang.equals("fr"))
+			ApplicationProperties.getInstance("C:\\Users\\Mike\\eclipse-workspace\\archiOOCalculatrice\\src\\welcome_fr.properties");
+		else
+			ApplicationProperties.getInstance("C:\\Users\\Mike\\eclipse-workspace\\archiOOCalculatrice\\src\\welcome_en.properties");
+		
+		System.out.println(ApplicationProperties.readProperty("welcome", "erreur"));
+		
+		
 		Interface i = new Interface();
 		while(true) {
 			
